242f24840b031f4df37d0d7cbeeb63ca920286d4
Marco Montagna <marcojoemontagna@gmail.com>
2015-03-15 12:36:07 -0700

    Syncing to master

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 RubyRailsSample::Application.routes.draw do
   root 'welcome#index'
 
-    match 'device/register/:uuid/:admin_key', :to => 'device#register', :format => false, :as => :register_device, :via => [:put]
-    match 'device/deregister/:uuid/:admin_key', :to => 'device#deregister', :format => false, :as => :deregister_devise, :via => [:put]
+    match 'device/register/:uuid/:admin_key', :to => 'device#register', :format => false, :as => :register_device, :via => [:put, :get]
+    match 'device/deregister/:uuid/:admin_key', :to => 'device#deregister', :format => false, :as => :deregister_devise, :via => [:put, :get]
 
-    match 'credentials/create', :to => 'user#create', :format => false, :as => :credentials_new, :via => [:post]
+    match 'credentials/create', :to => 'user#create', :format => false, :as => :credentials_new, :via => [:post, :get]
 
 end
